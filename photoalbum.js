0bfb9f954b8e482d119a5acf8d6a21d898128af5
Boris Faure <boris@fau.re>
2013-07-15 23:21:32 +0200

    add photoalbum.js

---
+++ photoalbum.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+var util = require('util');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
+
+
+var args = process.argv.splice(2);
+
+console.log(args);
