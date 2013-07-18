4ae178d0d1cfbad649e73f2208fdf180332a46e9
Boris Faure <boris@fau.re>
2013-07-18 18:26:39 +0200

    generate dummy index file

--- photoalbum.js
+++ photoalbum.js
@@ -4,6 +4,31 @@ var util = require('util');
 var os = require('os');
 var fs = require('fs');
 var path = require('path');
+var jade = require('jade');
+
+
+
+
+var genIndex = function (cfg) {
+    var j = fs.readFileSync('template/index.haml');
+
+    var fn = jade.compile(j);
+
+    var html = fn({pics: cfg.images});
+    console.log(html);
+
+    var indexPath = path.join(cfg.out, 'index.html');
+    fs.writeFile(indexPath, html, function(err) {
+        if (err) {
+            throw (err);
+        }
+    });
+};
+
+
+
+
+
 
 
 var args = process.argv.splice(2);
@@ -17,4 +42,8 @@ var usage = function() {
 if (args.length != 1) {
     usage();
 }
-console.log(args);
+
+var data = fs.readFileSync(args[0]);
+var cfg = JSON.parse(data);
+
+genIndex(cfg);
