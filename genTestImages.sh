01d720acc6be0f67f44923171cacfe19670b4ee6
Boris Faure <boris@fau.re>
2013-07-21 16:18:17 +0200

    add genTestImages.sh

---
+++ genTestImages.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: `basename $0` {images_to_generate}"
+    exit 1;
+fi
+
+for I in `seq 1 $1`
+do
+    convert -size 3072x2304 -gravity center label:$I in/IMG_$I.jpg
+done
